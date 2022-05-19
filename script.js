77e351ea9b953a400a06184827ebd9d4161ada13
unknown <loknathsds66@gmail.com>
2022-05-19 21:55:29 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function bulbToggle(){
+    let bulb = document.getElementById('lightbulb');
+
+    if(bulb.src.match('bulboff')){
+        bulb.src = 'bulbon.jpg';
+    } else {
+        bulb.src = 'bulboff.jpg'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
